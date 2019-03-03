2697c84cff428aceed0d83014baca725e35ec7ea
Sachin Pai <sachinspai04@gmail.com>
2019-03-03 21:10:42 +0530

    first commit

---
+++ Registration/src/main/java/com/spai/registration/RegistrationController.java
@@ -0,0 +1,8 @@
+package com.spai.registration;
+
+public class RegistrationController {
+
+	public static void main(String[] args) {
+		System.out.println("Hello i'm connecting to the World!");
+	}
+}
